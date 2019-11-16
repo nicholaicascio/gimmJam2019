1ae077f3a85b6c5cb6f3bc6cb162e2d27d489b75
nicholaicascio <lordcohliani@gmail.com>
2019-11-15 18:07:11 -0700

    initialize
    getting scene set up. going to be a game where you make root beer floats for people and accept their quirks

---
+++ whatever floats your boat/Assets/Scripts/DrinkButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkButton : MonoBehaviour
+{
+    public BoxCollider drinktable;
+    public GameObject sodaStream;
+    private bool sodaOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(collision.gameObject.ToString());
+    }
+
+    private void OnMouseDown()
+    {
+        if(!sodaOn)
+        {
+            sodaStream.gameObject.SetActive(true);
+            sodaOn = true;
+        }
+        else
+        {
+            sodaStream.gameObject.SetActive(false);
+            sodaOn = false;
+        }
+
+        //Debug.Log("Soda");
+    }
+}
